48c93dd78e5e19c20664a7c0968b4e901db16c06
Jess MacQueen <jess@getsentry.com>
2017-11-08 11:43:14 -0800

    basic chrome extension that injects raven-js, provides a form for dsn

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('form').addEventListener('submit', function(evt) {
+    evt.preventDefault();
+    var dsnEl = document.getElementById('dsn');
+    // TODO: save and refresh page
+  });
+});
+
